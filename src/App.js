d79ff7e6ffe272c8b6d6bb2da7f08b8ac428c9d7
tk14shiina <49159641+tk14shiina@users.noreply.github.com>
2022-05-13 14:39:42 +0700

    add handleFileTextSubmission and fix stuff

--- src/App.js
+++ src/App.js
@@ -21,30 +21,28 @@ const symtom_and_disease = "#4d000b";
 const transportation ="#e4b84a";
 function App(){
 	const [selectedFile, setSelectedFile] = useState();
-	const [isFilePicked, setIsFilePicked] = useState(false);
-	const [inputOCR, setInputOCR] = useState('Input rỗng');
+	const [inputOCR, setInputOCR] = useState('Nhập vào đây');
 	const [inputSubmitted, setInputSubmitted] = useState(false);
 	const [outputNER, setOutputNER] = useState();
+	let formData = new FormData();
 	const handleClearOutput = () => {
 		let newFormData = new FormData();
 		formData = newFormData;
-		setIsFilePicked(false);
-		setInputOCR('Input rỗng');
+		setInputOCR('Nhập vào đây');
 		setOutputNER();
 		setInputSubmitted(false);
+		setSelectedFile(null);
 	};
-	let formData = new FormData();
+	
 	const handleSelectedFile = (event) => {
 		setSelectedFile(event.target.files[0]);
-		setIsFilePicked(true);
 	};
 	const handleOCRSubmission = () => {
 		
 		console.log(selectedFile);
-		const formData = new FormData();
 		formData.append('file', selectedFile);
 		fetch(
-			'http://oner.buzzle.works:8000/OCR/file_upload',
+			'http://oner.buzzle.works:8000/ocr/file_upload',
 			{
 				method: 'POST',
 				body: formData
@@ -55,7 +53,7 @@ function App(){
 				let tempResult = '';
 				result.map(i => (
 					i.confidence > 80 ? 
-						tempResult += i.text : tempResult));
+						tempResult += (i.text + ' '): tempResult));
 				console.log(tempResult);
 				setInputOCR(tempResult);
 			})
@@ -63,37 +61,57 @@ function App(){
 				console.error('Error:', error);
 			});	
 	};
-	const handleNERSubmission = () => {
-		setInputSubmitted(true);
+	const handleFileTextSubmission = () => {
+		
+		console.log(selectedFile);
+		formData.append('file', selectedFile);
 		fetch(
-			'http://oner.buzzle.works:8000/NER/file_upload',
+			'http://oner.buzzle.works:8000/ner/file_upload',
 			{
 				method: 'POST',
-				body: inputOCR
+				body: formData
 			}
 		)
 			.then((response) => response.json())
 			.then((result) => {
-				setOutputNER(result);
+				let tempResult = [];
+				result.map(i => 
+						tempResult.push([i.token, i.prediction]));
+				setOutputNER(tempResult);
+				setInputSubmitted(true);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});	
+	};
+	const handleNERSubmission = () => {
+		fetch(
+			'http://oner.buzzle.works:8000/ner/text_upload',
+			{
+				method: 'POST',
+				headers: {'Content-Type': 'application/json;charset=utf-8'},
+				body: JSON.stringify({text: inputOCR})	
+			}
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				let tempResult = [];
+				result.map(i => 
+						tempResult.push([i.token, i.prediction]));
+				setOutputNER(tempResult);
+				setInputSubmitted(true);
 			})
 			.catch((error) => {
 				console.error('Error:', error);
 			});		
-
+			
 	};
 	return(
    		<div>
 			<Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 16 }}>
 				<Grid item spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 8 }}>
 				<Grid item xs={2} sm={4} md={8}>
-					<input type="file" name="file" onChange={handleSelectedFile} />
-					{isFilePicked ? (
-						<div>
-							<p></p>
-						</div>
-					) : (
-						<p>Select a file to show details</p>
-					)}
+					<input type="file" name="file" onChange={handleSelectedFile}/>
 				</Grid>
 				<Grid item xs={2} sm={4} md={8}>
 					<button onClick={handleOCRSubmission}>Submit OCR</button>
@@ -117,39 +135,39 @@ function App(){
 					<Box style={{border: "1px solid grey", wordWrap: "break-word", maxWidth: "900px", padding: "18px 18px"}}>
 					{
 						outputNER.map(i => 
-						<Switch condition={i.predictions.substring(2)}>
+						<Switch condition={i[1].substring(2)}>
 							<Case value={'PATIENT_ID'}>
-								<span style={{ color: patient_id }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: patient_id }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'NAME'}>
-								<span style={{ color: name }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: name }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'AGE'}>
-								<span style={{ color: age }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: age }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'GENDER'}>
-								<span style={{ color: gender }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: gender }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'JOB'}>
-								<span style={{ color: job }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: job }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'ORGANIZATION'}>
-								<span style={{ color: organization }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: organization }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'LOCATION'}>
-								<span style={{ color: location }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: location }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'DATE'}>
-								<span style={{ color: date }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: date }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'SYMTOM_AND_DISEASE'}>
-								<span style={{ color: symtom_and_disease }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: symtom_and_disease }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Case value={'TRANSPORTATION'}>
-								<span style={{ color: transportation }}> {i.tokens.replace('_', ' ')}</span>
+								<span style={{ color: transportation }}> {i[0].replace('_', ' ')}</span>
 							</Case>
 							<Default>
-								<span style={{ color: "black" }}> {i.tokens[0]==='<'? null:i.tokens}</span>
+								<span style={{ color: "black" }}> {i[0][0]==='<'? null:i[0].replace('_', ' ')}</span>
 							</Default>
 						</Switch>)
 					}
